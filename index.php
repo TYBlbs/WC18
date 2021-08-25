4b0f944c5337d9149e9280c4e0c6f733cab1b6ba
Tayeb Youcef <89312468+TYBlbs@users.noreply.github.com>
2021-08-25 23:24:13 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+
+<?php 
+        echo '<iframe allow="encrypted-media" width="700" height="480" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" allowfullscreen="yes" src="https://freefeds.click/stream/1/' .$_GET['f']. '.html"></iframe>'
+?>
+
+
+
+</body>
+</html>
